82266323829b212d9d703e99f1e252a410961765
NABAJYOTI BANIK <banik.nabajyoti2005@gmail.com>
2026-05-20 14:59:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.py
+++ Difficulty: Medium/Next Greater Element/next-greater-element.py
@@ -1,18 +1,18 @@
 class Solution:
     def nextLargerElement(self, arr):
-        stack = []
-        res = [-1] * len(arr)
 
-        for i in range(len(arr) - 1, -1, -1):
-            
+        n = len(arr)
+        res = [-1] * n
+        stack = []
+
+        for i in range(n - 1, -1, -1):
+
             while stack and stack[-1] <= arr[i]:
                 stack.pop()
 
-            
             if stack:
                 res[i] = stack[-1]
 
-            
             stack.append(arr[i])
 
         return res
