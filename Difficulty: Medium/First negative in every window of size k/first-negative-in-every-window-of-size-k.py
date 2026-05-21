c9d007ffc36750b7cfb773f3ede5867c06cf01c7
NABAJYOTI BANIK <banik.nabajyoti2005@gmail.com>
2026-05-21 14:19:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.py
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.py
@@ -3,28 +3,24 @@ from collections import deque
 class Solution:
     def firstNegInt(self, arr, k):
 
-        q = deque()
-        ans = []
-
-        for i in range(len(arr)):
-
-            
+        queue = deque()
+        for i in range(k):
+            if arr[i]<0:
+                queue.append(i)
+        result = []
+        if queue:
+            result.append(arr[queue[0]])
+        else:
+            result.append(0)
+        
+        for i in range(k, len(arr)):
+            while queue and queue[0]<=i-k:
+                queue.popleft()
             if arr[i] < 0:
-                q.append(i)
-
-            # Window starts after k-1
-            if i >= k - 1:
-
-                # Remove out of window indices
-                while q and q[0] < i - k + 1:
-                    q.popleft()
-
-                # First negative element
-                if q:
-                    ans.append(arr[q[0]])
-                else:
-                    ans.append(0)
-
-        return ans
-          
-          
+                queue.append(i)
+            if queue:
+                result.append(arr[queue[0]])
+            else:
+                result.append(0)
+        return result
+        
